222db4c1ed06ebe9605cebc96fd7b95d0f8fc568
Alexander Leurdo <Alexander_Leurdo@epam.com>
2023-08-13 14:17:32 +0400

    wip

--- iac/main.tf
+++ iac/main.tf
@@ -3,3 +3,31 @@ terraform {
   required_providers {
   }
 }
+
+resource "local_file" "myfile1" {
+  for_each = toset(["11","22","33"])
+  filename = "files/myfile_${each.key}.txt"
+  content = ""
+
+  # provisioner "local-exec" {
+  #   command = "echo ${each.key} > files/myfile_${each.key}.txt"
+  # }
+}
+
+# resource "null_resource" "updater" {
+#   for_each = toset(keys(local_file.myfile1))
+
+#   provisioner "local-exec" {
+#     command = "echo ${each.key} > files/myfile_${each.key}.txt"
+#   }
+# }
+
+output "names" {
+  value ={
+    for k,v in local_file.myfile1 : k => {"filename": v.filename, "content": v.content}
+  }
+}
+
+output "name1" {
+  value = tolist(values({"a": 1, "b": 2}))
+}
